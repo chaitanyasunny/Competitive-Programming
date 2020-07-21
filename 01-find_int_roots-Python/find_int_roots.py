be4365b7868f1a0eefc2bd28ce38b80e059d90b7
Chay <chaitanya.sunny7@gmail.com>
2020-07-21 19:02:11 -0400

    used a delta function

--- 01-find_int_roots-Python/find_int_roots.py
+++ 01-find_int_roots-Python/find_int_roots.py
@@ -8,15 +8,12 @@
 
 import math
 def fun_find_int_roots(a, b, c):
+	delta = b*b-4*a*c
 
-	x = ( -b + math.sqrt((b**2) - 4 * a * c))/ 2*a
-	y = ( -b - math.sqrt((b**2) - 4 * a * c))/ 2*a
+	x1=(math.sqrt(delta)-b)//(2*a)
 
-	if ( x >y):
-		return y,x
-	else:
-		return x,y
+	x2=-(math.sqrt(delta)+b)//(2*a)
 
-
-# ZeroDivisionError
-
+	if x1<x2:
+		return x1,x2
+	else:return x2,x1
