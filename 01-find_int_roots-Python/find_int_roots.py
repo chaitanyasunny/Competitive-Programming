ef7462661a523c995fa1a7c9d74764f37f69d35d
Chay <chaitanya.sunny7@gmail.com>
2020-07-21 19:06:39 -0400

    removed integer division

--- 01-find_int_roots-Python/find_int_roots.py
+++ 01-find_int_roots-Python/find_int_roots.py
@@ -9,11 +9,8 @@
 import math
 def fun_find_int_roots(a, b, c):
 	delta = b*b-4*a*c
-
-	x1=(math.sqrt(delta)-b)//(2*a)
-
-	x2=-(math.sqrt(delta)+b)//(2*a)
-
+	x1=(math.sqrt(delta)-b)/(2*a)
+	x2=-(math.sqrt(delta)+b)/(2*a)
 	if x1<x2:
 		return x1,x2
 	else:return x2,x1
